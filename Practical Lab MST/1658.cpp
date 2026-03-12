2fee251b4c14349d0e42631289be562231f6307a
Sumit Lakra <99547820+lakrasumit@users.noreply.github.com>
2026-03-12 23:28:52 +0530

    Lab MST

--- Practical Lab MST/1658.cpp
+++ Practical Lab MST/1658.cpp
@@ -3,16 +3,16 @@ public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
         int ans = INT_MAX;
-        for(int i=0; i<=n;i++) {
-            int leftSum=0;
-            for(int l = 0; l < i; l++)
-                leftSum += nums[l];
-            int rightSum = 0;
-            for(int j = 0; j <= n - i; j++) {
-                if(leftSum + rightSum == x)
+        for (int i = 0; i <= n;i++) {
+            int left = 0;
+            for (int l = 0; l < i; l++)
+                left += nums[l];
+            int right = 0;
+            for (int j = 0; j <= n - i; j++) {
+                if (left + right == x)
                     ans = min(ans, i + j);
-                if(j < n - i)
-                    rightSum += nums[n - 1 - j];
+                if (j < n - i)
+                    right += nums[n - 1 - j];
             }
         }
         if(ans == INT_MAX) return -1;
